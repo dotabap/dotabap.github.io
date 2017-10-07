6205f8620ffb80c5d35fbfff5fa4a5ec7ea81738
larshp <larshp@hotmail.com>
2017-10-07 13:50:59 +0000

    must be it for today

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
 let repos = [];
+let original;
 
-// todo, there must be some easier way to sort
-function sortByName() {
-  repos.sort((a, b) => {return a.name < b.name ? -1 : a.name > b.name;});
+function sortDescending(field) {
+  repos.sort((a, b) => {return a[field] > b[field] ? -1 : a[field] < b[field];});
+  render();
+}
+function sortAscending(field) {
+  repos.sort((a, b) => {return a[field] < b[field] ? -1 : a[field] > b[field];});
   render();
 }
 
-function sortByStars() {
-  repos.sort((a, b) => {return a.stars > b.stars ? -1 : a.stars < b.stars;});
-  render();
-}
+function search(e) {
+  if (e === "") {
+    repos = original;
+    render();
+    return;
+  }
+  repos = [];
+  for (let repo of original) {
+    if (!repo.description) continue;
+    console.dir(repo.description);
+    let regex = new RegExp(e, 'i');
+    if (repo.description.match(regex)) {
+      repos.push(repo);
+    }
+  }
 
-function sortByUpdated() {
-  repos.sort((a, b) => {return a.pushed_at > b.pushed_at ? -1 : a.pushed_at < b.pushed_at;});
-  render();
-}
-
-function sortByCreated() {
-  repos.sort((a, b) => {return a.created_at > b.created_at ? -1 : a.created_at < b.created_at;});
-  render();
-}
-
-function sortBySize() {
-  repos.sort((a, b) => {return a.lines > b.lines ? -1 : a.lines < b.lines;});
-  render();
-}
-
-function sortByAuthor() {
-  repos.sort((a, b) => {return a.owner < b.owner ? -1 : a.owner > b.owner;});
+// todo, this does not respect current sorting
   render();
 }
 
 function parse(json) {
   for(let name in json) {
     repos.push({name,
-      description: json[name].description,
-      git_url: json[name].git_url,
-      stars: json[name].github.stargazers_count,
-      html_url: json[name].github.html_url,
-      owner: json[name].github.owner.login,
-      owner_url: json[name].github.owner.html_url,
+      description: json[name].repo.description,
+      git_url: json[name].repo.clone_url,
+      stars: json[name].repo.stargazers_count,
+      html_url: json[name].repo.html_url,
+      owner: json[name].repo.owner.login,
+      owner_url: json[name].repo.owner.html_url,
       lines: json[name].lines,
-      created_at: new Date(json[name].github.created_at),
-      pushed_at: new Date(json[name].github.pushed_at)
-    })
+      created_at: new Date(json[name].repo.created_at),
+      pushed_at: new Date(json[name].repo.pushed_at)
+    });
   }
+  original = repos;
 }
 
 function render() {
   let html = "";
   for(let repo of repos) {
     html = html + "<div class=\"repo\">"+
-      "<h3><a href="+repo.html_url+">" + repo.name + "</a></h3>" +
+      "<h3><a href=" + repo.html_url + ">" + repo.name + "</a></h3>" +
       "<h4>" + repo.description + "</h4>" +
       "<small>" +
       "Git URL: " + repo.git_url + "<br>" +
-      "Owner: <a href=\""+repo.owner_url+"\">" + repo.owner + "</a><br>" +
+      "Owner: <a href=\"" + repo.owner_url + "\">" + repo.owner + "</a><br>" +
       "Lines: " + repo.lines + "<br>" +
       "Stars: " + repo.stars + "<br>" +
       "Updated: " + repo.pushed_at.toLocaleDateString() + "<br>" +
@@ -69,7 +69,7 @@ function render() {
 function callback(xhttp) {
   if (xhttp.readyState == 4 && xhttp.status == 200) {
     parse(JSON.parse(xhttp.responseText));
-    sortByUpdated();
+    sortDescending('pushed_at');
   }
 }
 
